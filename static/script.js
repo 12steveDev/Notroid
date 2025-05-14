d57afb3cc3e8d80f7fc7e007213370a17f1514dc
12steveDev <gcbismark@gmail.com>
2025-05-13 22:10:59 -0500

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,10 +1,13 @@
 // static/script.js
-// TODO: FIleSystem, AlertDialog, Notifications, SaveStateInLocalStorage, NavigationBar(currApp, register)
+// TODO: FileSystem, AlertDialog, Notifications, SaveStateInLocalStorage, NavigationBar(currApp, register)
+// TODO: Para INSTALL_APP: Si la app ya existe, pedir actualizar y manejar eso.
 const desktop = document.querySelector(".desktop");
+const randInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 let currApp = null;
 const permissions = JSON.parse(localStorage.getItem("__notroid_permissions__")) || {
     "notroid.permission.WRITE_STORAGE": ["NotasChafas"],
     "notroid.permission.READ_STORAGE": ["NotasChafas"],
+    "notroid.permission.INSTALL_APPS": ["NotroidStudio"],
     "notroid.permission.EXACT_IP_ACCESS🙏🤑🔥": [] // WTF 🙏☠🔥
 }
 const toastStack = []; // [msg]
@@ -226,6 +229,34 @@ const icons = JSON.parse(localStorage.getItem("__notroid_icons__")) || {
                 {type: "button", text: "Otras apps de entrada de texto", action: ["SEND_INTENT", "notroid.category.ENTRY_TEXT", "Introduce la nota: ", ["SET_TEXT", "noteInput", "$__intentResult"]]}
             ]
         }
+    },
+    "NotroidStudio": {
+        manifest: {
+            id: "NotroidStudio",
+            name: "Notroid Studio",
+            icon: "https://placehold.co/150x150/00cc99/FFFFFF?text=NS",
+            categories: ["notroid.category.CREATE_APP"],
+            permissions: ["notroid.permission.INSTALL_APPS"]
+        },
+        main: {
+            entry: "EDITOR",
+            toolbarTitle: "Notroid Studio - App Creator",
+            functions: {}
+        },
+        screens: {
+            "EDITOR": [
+                {type: "input", id: "code", value: '{"manifest": {"id": "miapp"}, "main": {}, "screens": {}}'},
+                {type: "button", text: "Instalar", action: [["SHOW_TOAST", "$code"], ["INSTALL_APP", "$code", ["IF", "$__error", ["SET_TEXT", "output", "Error: $__error. Arregla ese JSON bro."], ["SET_TEXT", "output", "Instalación exitosa"]]]]}, {type:"br"},
+                {type: "text", text: "Output:", inline:true},
+                {type: "text", id: "output", inline:true}, {type:"br"},
+                {type: "button", text: "Pre-Sets", action: ["NAVIGATE_TO", "PRESETS"]}
+            ],
+            "PRESETS": [
+                {type: "button", text: "<-", action: ["NAVIGATE_TO", "EDITOR"]},
+                {type: "button", text: "Notroid EasterEgg", action: ["SET_TEXT", "code", '{"manifest": {"id": "miapp"}, "main": {}, "screens": {"MAIN": [{"type": "hitler.update"}]}}']},
+                {type: "text", text: "Proximamente, pero es buena idea..."}
+            ]
+        }
     }
 };
 
@@ -332,7 +363,7 @@ function executeNotroid(appId, elem, actionArr){
                             if (Array.isArray(item)){
                                 return parseIntentResults(item, intentResult);
                             } else if (typeof item === "string"){
-                                return item.replace(/\$__intentResult/g, intentResult)
+                                return item.replace(/\$__intentResult/g, intentResult);
                             } else {
                                 return item;
                             }
@@ -355,6 +386,10 @@ function executeNotroid(appId, elem, actionArr){
             executeNotroid(appId, elem, actions)
             break;
         case "SAVE_ENV":
+            if (!icons[appId].manifest.permissions.includes("notroid.permission.WRITE_STORAGE")){
+                console.warn("[ExecuteNotroid] Acción desconocida (o falta permisos):", action);
+                return;
+            }
             if (!hasPermission(appId, "notroid.permission.WRITE_STORAGE")){
                 alert("Popup epiko que pida si quieres o no otorgarle permisos de escritura🔥🤑🤑🔥🔥🔥🗣🗣🔥🔥🤑🔥");
                 // TODO: Hacer un popup de confirmación si queremos otorgarle permisos
@@ -363,6 +398,10 @@ function executeNotroid(appId, elem, actionArr){
             localStorage.setItem(`env-${appId}-${args[0]}`, resolveValue(appId, args[1]));
             break;
         case "LOAD_ENV":
+            if (!icons[appId].manifest.permissions.includes("notroid.permission.READ_STORAGE")){
+                console.warn("[ExecuteNotroid] Acción desconocida (o falta permisos):", action);
+                return;
+            }
             if (!hasPermission(appId, "notroid.permission.READ_STORAGE")){
                 alert("Popup epiko que pida si quieres o no otorgarle permisos de lectura🔥🤑🤑🔥🔥🔥🗣🗣🔥🔥🤑🔥");
                 // TODO: Hacer un popup de confirmación si queremos otorgarle permisos
@@ -370,84 +409,156 @@ function executeNotroid(appId, elem, actionArr){
             }
             icons[appId].main.env[args[1]] = localStorage.getItem(`env-${appId}-${args[0]}`) || "";
             break;
+        case "INSTALL_APP":
+            if (!icons[appId].manifest.permissions.includes("notroid.permission.INSTALL_APPS")){
+                console.warn("[ExecuteNotroid] Acción desconocida (o falta permisos):", action);
+                return;
+            }
+            if (!hasPermission(appId, "notroid.permission.INSTALL_APPS")){
+                alert("Popup epiko que pida si quieres o no otorgarle permisos de instalar apps de dudosa procedencia🔥🤑🤑🔥🔥🔥🗣🗣🔥🔥🤑🔥");
+                // TODO: Hacer un popup de confirmación si queremos otorgarle permisos
+                return;
+            }
+            function parseIntentResults(actions, intentResult){
+                if (Array.isArray(actions)){
+                    return actions.map(item => {
+                        if (Array.isArray(item)){
+                            return parseIntentResults(item, intentResult);
+                        } else if (typeof item === "string"){
+                            return item.replace(/\$__error/g, intentResult);
+                        } else {
+                            return item;
+                        }
+                    });
+                }
+                return actions;
+            }
+            try {
+                appObj = JSON.parse(resolveValue(appId, args[0]));
+                installApp(appObj, add=true);
+                executeNotroid(appId, null, parseIntentResults(args[1] || [], ""));
+            } catch (error){
+                executeNotroid(appId, null, parseIntentResults(args[1] || [], error.message));
+                break;
+            }
+            break;
         default:
             console.warn("[ExecuteNotroid] Acción desconocida:", action);
             break;
     }
 }
+function installApp(appObj, add=false){
+    if (!appObj.manifest || !appObj.main || !appObj.screens) throw new SyntaxError("Estructura de app incompleta");
+    if (!appObj.manifest.id) throw new SyntaxError("Falta el ID único de la app");
+    if (!appObj.manifest.name) appObj.manifest.name = appObj.manifest.id;
+    if (!appObj.manifest.icon) appObj.manifest.icon = `https://placehold.co/150x150/008080/FFFFFF?text=My\\nNot-App`;
+    if (!appObj.manifest.categories) appObj.manifest.categories = [];
+    if (!appObj.manifest.permissions) appObj.manifest.permissions = [];
+    if (!appObj.main.toolbarTitle) appObj.main.toolbarTitle = appObj.manifest.name;
+    if (!appObj.main.functions) appObj.main.functions = {};
+    if (!appObj.main.lifecycle) appObj.main.lifecycle = {onCreate: [], onDestroy: []};
+    if (!appObj.main.lifecycle.onCreate) appObj.main.lifecycle.onCreate = [];
+    if (!appObj.main.lifecycle.onDestroy) appObj.main.lifecycle.onDestroy = [];
+    if (!appObj.main.env) appObj.main.env = {};
+    if (Object.keys(appObj.screens).length === 0) appObj.screens = {"MAIN": []};
+    if (!appObj.main.entry) appObj.main.entry = Object.keys(appObj.screens)[0];
+    if (add) icons[appObj.manifest.id] = appObj;
+    //console.log(JSON.stringify(appObj, null, 4)); // activar solo en casos de desesperación
+    const icon = document.createElement("div");
+    icon.className = "icon";
+    const img = document.createElement("img");
+    img.src = appObj.manifest.icon;
+    img.alt = appObj.manifest.id;
+    const label = document.createElement("p");
+    label.textContent = appObj.manifest.name;
+    icon.appendChild(img);
+    icon.appendChild(label);
+    icon.onclick = ()=>{
+        launchApp(appObj.manifest.id, reset=true);
+    }
+    desktop.appendChild(icon);
+
+    const app = document.createElement("div");
+    app.className = "app hide";
+    app.id = `${appObj.manifest.id}-app`;
+    const toolbar = document.createElement("div");
+    toolbar.className = "toolbar";
+    toolbar.textContent = appObj.main.toolbarTitle || appObj.manifest.name;
+    app.appendChild(toolbar)
+    for (const [scrName, elements] of Object.entries(appObj.screens)){
+        const screen = document.createElement("div");
+        screen.className = "screen hide";
+        screen.id = `${scrName}-screen`;
+        for (const element of elements){
+            let elem;
+            switch (element.type){
+                case "text":
+                    elem = document.createElement("p");
+                    elem.textContent = element.text || "";
+                    break;
+                case "button":
+                    elem = document.createElement("button");
+                    elem.textContent = element.text || "Botón";
+                    break;
+                case "input":
+                    elem = document.createElement("input");
+                    elem.placeholder = element.placeholder || "";
+                    elem.value = element.value || "";
+                    break;
+                case "checkbox":
+                    elem = document.createElement("input");
+                    elem.type = "checkbox";
+                    elem.checked = element.checked || false;
+                    break;
+                case "br":
+                    screen.appendChild(document.createElement("br"));
+                    continue;
+                case "hitler.update":
+                    screen.querySelectorAll("*").forEach(elem=>{
+                        setInterval(()=>{
+                            if (elem.textContent) elem.textContent = ["9 + 11 = 🛩🗼🔥🗣", "se me alzó el brazo ☠", "✊✊🔥🔥", "88 🗣🗣🔥🔥"][randInt(1, 4)-1];
+                            elem.style.background = ["red", "green", "blue", "orange", "black", "white"][randInt(1, 6)-1];
+                            elem.style.color = ["red", "green", "blue", "orange", "black", "white"][randInt(1, 6)-1]
+                            elem.onclick = ()=> elem.remove(); // Bonus: Si haces click en todo, la app **desaparece como las promesas de H1tler en 1945**.
+                        }, 500);
+                    });
+                    toolbar.textContent = ["🗣🔥🔥", "🛩🗼🔥"][randInt(1, 2)-1];
+                    const hh = ["AdolfOS 2.0", "Notzi Beta", "911HH"][randInt(1, 3)-1];
+                    appObj.manifest.name = hh;
+                    label.textContent = hh;
+                    appObj.manifest.name = "";
+                    elem = document.createElement("input");
+                    elem.type = "range";
+                    elem.value = 88; // porque 88 = H H = "Hei..." 😭🛑
+                    elem.title = "HH";
+                    elem.oninput = ()=> BSOD()
+                    break;
+                default:
+                    console.warn("Elemento desconocido:", element);
+                    elem = document.createElement("div");
+                    elem.textContent = `[Elemento no soportado: ${element.type}]`;
+                    break;
+            }
+            if (element.id){
+                elem.id = `specid-${appObj.manifest.id}-${element.id}`;
+            }
+            if (element.inline === true){
+                elem.style.display = "inline";
+            }
+            if (element.action){
+                elem.onclick = ()=>{
+                    executeNotroid(appObj.manifest.id, elem, element.action)
+                }
+            }
+            screen.appendChild(elem);
+        }
+        app.appendChild(screen);
+    }
+    desktop.appendChild(app);
+}
 function loadIcons(){
     for (const appObj of Object.values(icons)){
-        const icon = document.createElement("div");
-        icon.className = "icon";
-        const img = document.createElement("img");
-        img.src = appObj.manifest.icon;
-        img.alt = appObj.manifest.id;
-        const label = document.createElement("p");
-        label.textContent = appObj.manifest.name;
-        icon.appendChild(img);
-        icon.appendChild(label)
-        icon.onclick = ()=>{
-            launchApp(appObj.manifest.id, reset=true)
-        }
-        desktop.appendChild(icon)
-
-        const app = document.createElement("div");
-        app.className = "app hide";
-        app.id = `${appObj.manifest.id}-app`;
-        const toolbar = document.createElement("div");
-        toolbar.className = "toolbar";
-        toolbar.textContent = appObj.main.toolbarTitle || appObj.manifest.name;
-        app.appendChild(toolbar)
-        for (const [scrName, elements] of Object.entries(appObj.screens)){
-            const screen = document.createElement("div");
-            screen.className = "screen hide";
-            screen.id = `${scrName}-screen`;
-            for (const element of elements){
-                let elem;
-                switch (element.type){
-                    case "text":
-                        elem = document.createElement("p");
-                        elem.textContent = element.text || "";
-                        break;
-                    case "button":
-                        elem = document.createElement("button");
-                        elem.textContent = element.text || "Botón";
-                        break;
-                    case "input":
-                        elem = document.createElement("input");
-                        elem.placeholder = element.placeholder || "";
-                        elem.value = element.value || "";
-                        break;
-                    case "checkbox":
-                        elem = document.createElement("input");
-                        elem.type = "checkbox";
-                        elem.checked = element.checked || false;
-                        break;
-                    case "br":
-                        screen.appendChild(document.createElement("br"));
-                        continue;
-                    default:
-                        console.warn("Elemento desconocido:", element);
-                        elem = document.createElement("div");
-                        elem.textContent = `[Elemento no soportado: ${element.type}]`;
-                        break;
-                }
-                if (element.id){
-                    elem.id = `specid-${appObj.manifest.id}-${element.id}`;
-                }
-                if (element.inline === true){
-                    elem.style.display = "inline";
-                }
-                if (element.action){
-                    elem.onclick = ()=>{
-                        executeNotroid(appObj.manifest.id, elem, element.action)
-                    }
-                }
-                screen.appendChild(elem);
-            }
-            app.appendChild(screen);
-        }
-        desktop.appendChild(app);
+        installApp(appObj);
     }
 }
 // ========== INICIALIZACIÓN DE APPS ========== //
@@ -470,8 +581,8 @@ function launchApp(appId, reset=false, data=null){
         app.style.display = "block";
         void app.offsetWidth; // Tremendo hack, campeón
         app.classList.remove("hide");
-        executeNotroid(appId, app, appObj.main.lifecycle.onCreate);
-    }, 200)
+        executeNotroid(appId, app, appObj.main.lifecycle?.onCreate || []);
+    }, 200);
 }
 function closeApp(appId){
     const app = document.getElementById(`${appId}-app`);
@@ -482,11 +593,11 @@ function closeApp(appId){
     }
     setTimeout(()=>{
         app.classList.add("hide");
-        executeNotroid(appId, app, appObj.main.lifecycle.onDestroy);
+        executeNotroid(appId, app, appObj.main.lifecycle?.onDestroy || []);
         setTimeout(()=>{
             app.style.display = "none";
         }, 400);
-    }, 200)
+    }, 200);
 }
 function navigateTo(context, screenId){
     const app = context.closest(".app");
@@ -542,6 +653,29 @@ function showAlertDialog(appId, title, msg, positive, negative){ // Perdonenme, 
         setTimeout(()=>{ alertDialog.style.display = "none" }, 400);
         executeNotroid(appId, null, negative[1] || []);
     }
+}
+function BSOD(){
+    document.body.innerHTML = "";
+    document.body.style.background = "#0078b7";
+    document.body.style.color = "white";
+    const h1 = document.createElement("h1");
+    h1.textContent = ":(";
+    h1.style.fontSize = "40px";
+    h1.style.padding = "50px";
+    document.body.appendChild(h1);
+    const p = document.createElement("p");
+    p.textContent = "No preguntes cómo arreglar esto, preguntate qué hiciste para llegar aquí.";
+    p.style.padding = "0 50px";
+    document.body.appendChild(p);
+    const btn = document.createElement("button");
+    btn.textContent = "Reiniciar";
+    btn.style.background = "none";
+    btn.style.border = "none";
+    btn.style.color = "white";
+    btn.style.margin = "50px";
+    btn.style.textDecoration = "underline";
+    btn.onclick = ()=>window.location.reload();
+    document.body.appendChild(btn);
 }
 // ========== UTILES ========== //  
 function resolveId(appId, id){
